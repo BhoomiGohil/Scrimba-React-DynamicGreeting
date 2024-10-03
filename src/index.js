89aab54873f31a7b50c8e4a7b7749382204f603f
Bhoomi Gohil <26293804+BhoomiGohil@users.noreply.github.com>
2024-10-03 13:44:32 -0400

    Updating layout

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-import MainContent from "./Practice2/MainContent";
+import MainContent from "./js/MainContent";
+import "./styles/index.css";
 
 // ------------------------------------- Render Elements on browser -------------------------------------
+// Select the root element from the DOM
 const rootElement = document.querySelector("#root");
 
+// Throw an error if the root element is not found
 if (!rootElement) throw new Error("Failed to find the root h1");
 
+// Create a root for rendering the React component
 const root = createRoot(rootElement);
 
+// Render the MainContent component inside the root element
 root.render(<MainContent />);
--- src/js/MainContent.js
+++ src/js/MainContent.js
@@ -1,12 +1,15 @@
 function MainContent() {
-  const firstName = "Bhoomi";
-  const lastName = "Gohil";
+  // Define user's first and last name
+  const firstName = "First and";
+  const lastName = "Last Name";
 
+  // Get the current date and extract the hour
   const date = new Date();
   const hours = date.getHours();
 
   let timeOfDay;
 
+  // Determine the time of day based on the current hour
   if (hours < 12) {
     timeOfDay = "morning";
   } else if (hours >= 12 && hours < 17) {
@@ -15,8 +18,9 @@ function MainContent() {
     timeOfDay = "night";
   }
 
+  // Return JSX displaying a greeting message and the current hour
   return (
-    <div>
+    <div className="container">
       <h1>
         Good {timeOfDay} {firstName} {lastName}!
       </h1>
